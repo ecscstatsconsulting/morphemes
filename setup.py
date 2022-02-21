118b25373fee5e1ee8db879c38daa1b99b198131
Paul Warren <paul0warren3@gmail.com>
2022-02-20 23:23:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     name='morphemes',
     long_description_content_type='text/markdown',
     long_description=long_description,
-    version='1.0.5',
+    version='1.0.6',
     packages=['morphemes', 'morphemes.utilities', 'morphemes.utilities.morpheme_database'],
     url='',
     license='MIT',
-    author='Paul Warren, Enkeleda Cuko',
-    author_email='ecsctechdepartment@gmail.com, ecsctechdepartment@gmail.com',
+    author='Enkeleda Cuko & Paul Warren',
+    author_email='ecsctechdepartment@gmail.com',
     description="""A practical Python Library for identifying morphemes in the english language."""
 )
